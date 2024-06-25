3fb6557e2826481c86c708bec42641f70207bc79
YASHasvi SHUkla <yashasvigot@gmail.com>
2024-06-25 11:19:42 +0530

    yayaya

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import profile from './profile_pic.jpg'
+import { createClient } from "@supabase/supabase-js";
+import Icon from './Icon'
 
 function App() {
+
+  const supabase = createClient(process.env.REACT_APP_SUPABASE_PRO_URL, process.env.REACT_APP_SUPABASE_PRO_KEY);
+  const [loading, setLoading] = useState(false)
+  const [links, setLinks] = useState([])
+
+  useEffect(() => {
+    async function getLinks() {
+      setLoading(true)
+      try {
+        const linksData = await supabase.from("links").select();
+        setLinks(linksData.data)
+        setLoading(false)
+      }
+      catch (e) {
+        console.log(e);
+        setLoading(false)
+      }
+    }
+
+    getLinks()
+  }, [])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header container mx-auto">
+        <img src={profile} className="profile-pic w-20 h-20 mb-4 rounded-full" alt="logo" />
+        {
+          loading ?
+            <p>Please wait I get my links ready</p>
+            :
+            <ul className='flex flex-col w-full '>
+              {
+                links && links.map((link, index) => (
+                  <li key={index} className='cursor-pointer m-2 py-3 px-10 border-black border-2 text-sm md:text-md text-yellow-white '>
+                    <a href={link.url} className='flex items-center' title={link.title} target='_blank' rel='noreferrer'>
+                      <Icon iconName={link.icon} /> <p className='ml-3'>{link.title}</p>
+                    </a>
+                  </li>
+                ))
+              }
+            </ul>
+        }
+
+      </div>
     </div>
   );
 }
